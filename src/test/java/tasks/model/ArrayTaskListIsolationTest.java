519f639bb7e8c4ff13fe500978b704adfd5669a8
Catalin Halbes <catalin.halbes@stud.ubbcluj.ro>
2023-05-09 17:23:52 +0300

    Created test classes for lab4

---
+++ src/test/java/tasks/model/ArrayTaskListIsolationTest.java
@@ -0,0 +1,7 @@
+package tasks.model;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ArrayTaskListIsolationTest {
+
+}
---
+++ src/test/java/tasks/utils/TasksUtilsIsolationTest.java
@@ -0,0 +1,7 @@
+package tasks.utils;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class TasksUtilsIsolationTest {
+
+}
