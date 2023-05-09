034c4f02d1a1886faa6601da44dff4def1b79d0f
Rares <raresgherman46@yahoo.com>
2023-05-09 17:39:41 +0300

    Created task tests

--- src/test/java/tasks/model/TaskIsolationTest.java
+++ src/test/java/tasks/model/TaskIsolationTest.java
@@ -1,7 +1,41 @@
 package tasks.model;
 
+import org.junit.jupiter.api.Test;
+
+import java.util.Calendar;
+import java.util.Date;
+import java.util.GregorianCalendar;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class TaskIsolationTest {
+
+    @Test
+    void testCreate_Task() {
+        // arrange
+        Date date = new GregorianCalendar(2023, Calendar.MARCH, 31).getTime();
+        Task task = new Task("task1", date);
+
+        // act
+
+        // assert
+        assertEquals(date, task.getTime() );
+        assertEquals("task1", task.getTitle());
+    }
+
+    @Test
+    void testSetTime_Task() {
+        // arrange
+        Date date = new GregorianCalendar(2023, Calendar.MARCH, 31).getTime();
+        Task task = new Task("task1", date);
+
+        // act
+
+        task.setActive(true);
+
+        // assert
+        assertTrue(task.isActive());
+        
+    }
 
 }
